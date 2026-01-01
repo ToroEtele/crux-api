8069b72e294a10301d73ecd8483d1ff1b67c8585
Toro Etele <toroetele@gmail.com>
2026-01-01 17:23:36 +0200

    commit

--- src/app/entities/workout-exercise/workout-exercise.resolver.ts
+++ src/app/entities/workout-exercise/workout-exercise.resolver.ts
@@ -4,14 +4,16 @@ import { Service } from 'typedi';
 
 import { AuthorizedAdmin } from '@app/access-control/authorization/authorized-admin.decorator';
 import { InjectRepository } from '@entity-management/decorators/inject-repository.decorator';
-import { InjectScoped } from '@app/access-control/scoping/inject-scoped.decorator';
+import { InjectScoped, MaybeInjectScopedOrThrow } from '@app/access-control/scoping/inject-scoped.decorator';
 import { ObjectId } from '../_common/object-id/object-id';
 
+import { PlanWorkoutExerciseSetOverride } from '../plan-workout-exercise-set-override/plan-workout-exercise-set-override.entity';
 import { WorkoutExerciseSet } from '../workout-exercise-set/workout-exercise-set.entity';
 import { WorkoutExercise } from './workout-exercise.entity';
 import { Exercise } from '../exercise/exercise.entity';
 import { Workout } from '../workout/workout.entity';
 
+import { PlanWorkoutExerciseOverrideRepository } from '../plan-workout-exercise-set-override/plan-workout-exercise-set-override.reporisory';
 import { WorkoutExerciseSetRepository } from '../workout-exercise-set/workout-exercise-set.repository';
 import { WorkoutExerciseRepository } from './workout-exercise.repository';
 import { ExerciseRepository } from '../exercise/exercise.repository';
@@ -20,6 +22,7 @@ import { WorkoutExerciseBaseResolver } from '../_generated/entity-base-resolvers
 
 import { CreateWorkoutExerciseInput } from './types/create-workout-exercise.input-type';
 import { UpdateWorkoutExerciseInput } from './types/update-workout-exercise.input-type';
+import { Plan } from '../plan/plan.entity';
 
 @Service()
 @Resolver((_of) => WorkoutExercise)
@@ -30,7 +33,9 @@ export class WorkoutExerciseResolver extends WorkoutExerciseBaseResolver {
     @InjectRepository(Exercise)
     private exerciseRepository: ExerciseRepository,
     @InjectRepository(WorkoutExerciseSet)
-    private setRepository: WorkoutExerciseSetRepository
+    private setRepository: WorkoutExerciseSetRepository,
+    @InjectRepository(PlanWorkoutExerciseSetOverride)
+    private overridesRepository: PlanWorkoutExerciseOverrideRepository
   ) {
     super(repository);
   }
@@ -65,10 +70,22 @@ export class WorkoutExerciseResolver extends WorkoutExerciseBaseResolver {
   }
 
   @FieldResolver((_type) => [WorkoutExerciseSet])
-  async sets(@Root() workoutExercise: WorkoutExercise): Promise<WorkoutExerciseSet[]> {
-    return await this.setRepository
+  async sets(
+    @Root() workoutExercise: WorkoutExercise,
+    @Arg('planId', (_type) => ObjectId, { nullable: true }) planId?: ObjectId | null,
+    @MaybeInjectScopedOrThrow('planId.id', Plan) plan?: Plan | null
+  ): Promise<WorkoutExerciseSet[]> {
+    const sets = await this.setRepository
       .createQueryBuilder()
       .where({ workoutExerciseId: { eq: workoutExercise.id } })
       .getMany();
+
+    if (!planId) return sets;
+
+    const overridesQuery = this.overridesRepository.createQueryBuilder().where({ workoutExerciseSetId: { in: sets.map((set) => set.id) } });
+    overridesQuery.builder.innerJoin('planWorkoutExerciseSetOverride.planWorkout', 'planWorkout', 'planWorkout.planId = :planId', { planId });
+    const overrides = await overridesQuery.getMany();
+
+    return sets.map((set) => ({ ...set, ...overrides.find((o) => o.workoutExerciseSetId === set.id || {}) }));
   }
 }
