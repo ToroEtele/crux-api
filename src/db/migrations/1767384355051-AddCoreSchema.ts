18728ed69820474aa31fc50adb21cc63d41c2d8e
Toro Etele <toroetele@gmail.com>
2026-01-02 22:09:18 +0200

    Fix migrations

--- src/db/migrations/1767384355051-AddCoreSchema.ts
+++ src/db/migrations/1767384355051-AddCoreSchema.ts
@@ -1,13 +1,9 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class AddCoreSchema1767380042595 implements MigrationInterface {
-    name = 'AddCoreSchema1767380042595'
+export class AddCoreSchema1767384355051 implements MigrationInterface {
+    name = 'AddCoreSchema1767384355051'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE \`avatar_image\` (\`id\` int NOT NULL AUTO_INCREMENT, \`bytes\` text NOT NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
-        await queryRunner.query(`CREATE TABLE \`account\` (\`id\` int NOT NULL AUTO_INCREMENT, \`access_token\` varchar(255) NULL, \`refresh_token\` varchar(255) NULL, \`id_token\` varchar(255) NULL, \`access_token_expires_at\` datetime NULL, \`refresh_token_expires_at\` datetime NULL, \`scope\` varchar(255) NULL, \`password\` varchar(255) NULL, \`created_at\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`updated_at\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), \`account_id\` varchar(255) NOT NULL, \`provider_id\` varchar(255) NOT NULL, \`user_id\` int NOT NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
-        await queryRunner.query(`CREATE TABLE \`user\` (\`id\` int NOT NULL AUTO_INCREMENT, \`name\` varchar(255) NOT NULL, \`email\` varchar(255) NOT NULL, \`email_verified\` tinyint NOT NULL DEFAULT 0, \`image\` varchar(255) NULL, \`created_at\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`updated_at\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), \`stripe_customer_id\` varchar(255) NULL, \`ban_expires\` varchar(255) NULL, \`ban_reason\` text NULL, \`banned\` tinyint NOT NULL DEFAULT 0, \`role\` text NULL, \`birth_date\` datetime NULL, \`country\` varchar(255) NULL, \`city\` varchar(255) NULL, \`max_grade_at_registration\` varchar(255) NULL, \`max_grade\` varchar(255) NULL, \`terms_agreed\` tinyint NOT NULL DEFAULT 0, \`avatar_image_id\` int NULL, UNIQUE INDEX \`IDX_e12875dfb3b1d92d7d7c5377e2\` (\`email\`), UNIQUE INDEX \`REL_013c0803e80018bfe46fca1a98\` (\`avatar_image_id\`), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
-        await queryRunner.query(`CREATE TABLE \`session\` (\`id\` int NOT NULL AUTO_INCREMENT, \`expires_at\` datetime NOT NULL, \`token\` varchar(255) NOT NULL, \`ip_address\` varchar(255) NULL, \`user_agent\` varchar(255) NULL, \`created_at\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`updated_at\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), \`impersonated_by\` text NULL, \`user_id\` int NOT NULL, UNIQUE INDEX \`IDX_232f8e85d7633bd6ddfad42169\` (\`token\`), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
         await queryRunner.query(`CREATE TABLE \`plan\` (\`id\` int NOT NULL AUTO_INCREMENT, \`name\` varchar(255) NOT NULL, \`description\` varchar(2048) NOT NULL, \`duration_weeks\` int NOT NULL, \`diff\` varchar(255) NOT NULL, \`is_public\` tinyint NOT NULL DEFAULT 0, \`category_id\` int NOT NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
         await queryRunner.query(`CREATE TABLE \`plan_workout\` (\`id\` int NOT NULL AUTO_INCREMENT, \`week_number\` int NOT NULL, \`day_of_week\` int NOT NULL, \`order_in_day\` int NULL, \`custom_name\` varchar(255) NULL, \`notes\` text NULL, \`workout_id\` int NOT NULL, \`plan_id\` int NOT NULL, UNIQUE INDEX \`UQ_PLAN_WORKOUT_ORDER\` (\`plan_id\`, \`week_number\`, \`day_of_week\`, \`order_in_day\`), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
         await queryRunner.query(`CREATE TABLE \`plan_workout_exercise_set_override\` (\`id\` int NOT NULL AUTO_INCREMENT, \`order\` int NOT NULL, \`reps\` int NULL, \`time\` int NULL, \`weight\` int NULL, \`rest_after\` int NULL, \`notes\` text NULL, \`plan_workout_id\` int NOT NULL, \`workout_exercise_set_id\` int NULL, \`workout_exercise_id\` int NOT NULL, INDEX \`IDX_3c8271e8e551601fe9e86e2c3d\` (\`workout_exercise_set_id\`), INDEX \`IDX_f2702fadc274e6c8530ee9982d\` (\`plan_workout_id\`), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
@@ -16,11 +12,11 @@ export class AddCoreSchema1767380042595 implements MigrationInterface {
         await queryRunner.query(`CREATE TABLE \`exercise\` (\`id\` int NOT NULL AUTO_INCREMENT, \`name\` varchar(255) NOT NULL, \`description\` varchar(2048) NOT NULL, \`diff\` varchar(255) NOT NULL, \`category_id\` int NOT NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
         await queryRunner.query(`CREATE TABLE \`category\` (\`id\` int NOT NULL AUTO_INCREMENT, \`name\` varchar(255) NOT NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
         await queryRunner.query(`CREATE TABLE \`workout\` (\`id\` int NOT NULL AUTO_INCREMENT, \`name\` varchar(255) NOT NULL, \`description\` varchar(2048) NOT NULL, \`diff\` varchar(255) NOT NULL, \`is_public\` tinyint NOT NULL DEFAULT 0, \`category_id\` int NOT NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
-        await queryRunner.query(`CREATE TABLE \`subscription\` (\`id\` int NOT NULL AUTO_INCREMENT, \`plan\` varchar(255) NOT NULL, \`reference_id\` varchar(255) NOT NULL, \`stripe_customer_id\` varchar(255) NULL, \`stripe_subscription_id\` varchar(255) NULL, \`status\` varchar(255) NOT NULL, \`period_start\` bigint NULL, \`period_end\` bigint NULL, \`cancel_at_period_end\` tinyint NULL, \`seats\` bigint NULL, \`trial_start\` datetime NULL, \`trial_end\` datetime NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
-        await queryRunner.query(`CREATE TABLE \`verification_token\` (\`id\` int NOT NULL AUTO_INCREMENT, \`identifier\` varchar(255) NOT NULL, \`value\` varchar(255) NOT NULL, \`expires_at\` datetime NOT NULL, \`created_at\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`updated_at\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
-        await queryRunner.query(`ALTER TABLE \`account\` ADD CONSTRAINT \`FK_efef1e5fdbe318a379c06678c51\` FOREIGN KEY (\`user_id\`) REFERENCES \`user\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
-        await queryRunner.query(`ALTER TABLE \`user\` ADD CONSTRAINT \`FK_013c0803e80018bfe46fca1a985\` FOREIGN KEY (\`avatar_image_id\`) REFERENCES \`avatar_image\`(\`id\`) ON DELETE SET NULL ON UPDATE NO ACTION`);
-        await queryRunner.query(`ALTER TABLE \`session\` ADD CONSTRAINT \`FK_30e98e8746699fb9af235410aff\` FOREIGN KEY (\`user_id\`) REFERENCES \`user\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE \`user\` ADD \`birth_date\` datetime NULL`);
+        await queryRunner.query(`ALTER TABLE \`user\` ADD \`country\` varchar(255) NULL`);
+        await queryRunner.query(`ALTER TABLE \`user\` ADD \`city\` varchar(255) NULL`);
+        await queryRunner.query(`ALTER TABLE \`user\` ADD \`max_grade_at_registration\` varchar(255) NULL`);
+        await queryRunner.query(`ALTER TABLE \`user\` ADD \`max_grade\` varchar(255) NULL`);
         await queryRunner.query(`ALTER TABLE \`plan\` ADD CONSTRAINT \`FK_4abedc01b0c0c400accca270efa\` FOREIGN KEY (\`category_id\`) REFERENCES \`category\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE \`plan_workout\` ADD CONSTRAINT \`FK_fa674f69afec2cedb26a5d20f9e\` FOREIGN KEY (\`workout_id\`) REFERENCES \`workout\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE \`plan_workout\` ADD CONSTRAINT \`FK_614784b0f5598a66d4ab7d16995\` FOREIGN KEY (\`plan_id\`) REFERENCES \`plan\`(\`id\`) ON DELETE CASCADE ON UPDATE NO ACTION`);
@@ -46,11 +42,11 @@ export class AddCoreSchema1767380042595 implements MigrationInterface {
         await queryRunner.query(`ALTER TABLE \`plan_workout\` DROP FOREIGN KEY \`FK_614784b0f5598a66d4ab7d16995\``);
         await queryRunner.query(`ALTER TABLE \`plan_workout\` DROP FOREIGN KEY \`FK_fa674f69afec2cedb26a5d20f9e\``);
         await queryRunner.query(`ALTER TABLE \`plan\` DROP FOREIGN KEY \`FK_4abedc01b0c0c400accca270efa\``);
-        await queryRunner.query(`ALTER TABLE \`session\` DROP FOREIGN KEY \`FK_30e98e8746699fb9af235410aff\``);
-        await queryRunner.query(`ALTER TABLE \`user\` DROP FOREIGN KEY \`FK_013c0803e80018bfe46fca1a985\``);
-        await queryRunner.query(`ALTER TABLE \`account\` DROP FOREIGN KEY \`FK_efef1e5fdbe318a379c06678c51\``);
-        await queryRunner.query(`DROP TABLE \`verification_token\``);
-        await queryRunner.query(`DROP TABLE \`subscription\``);
+        await queryRunner.query(`ALTER TABLE \`user\` DROP COLUMN \`max_grade\``);
+        await queryRunner.query(`ALTER TABLE \`user\` DROP COLUMN \`max_grade_at_registration\``);
+        await queryRunner.query(`ALTER TABLE \`user\` DROP COLUMN \`city\``);
+        await queryRunner.query(`ALTER TABLE \`user\` DROP COLUMN \`country\``);
+        await queryRunner.query(`ALTER TABLE \`user\` DROP COLUMN \`birth_date\``);
         await queryRunner.query(`DROP TABLE \`workout\``);
         await queryRunner.query(`DROP TABLE \`category\``);
         await queryRunner.query(`DROP TABLE \`exercise\``);
@@ -64,13 +60,6 @@ export class AddCoreSchema1767380042595 implements MigrationInterface {
         await queryRunner.query(`DROP INDEX \`UQ_PLAN_WORKOUT_ORDER\` ON \`plan_workout\``);
         await queryRunner.query(`DROP TABLE \`plan_workout\``);
         await queryRunner.query(`DROP TABLE \`plan\``);
-        await queryRunner.query(`DROP INDEX \`IDX_232f8e85d7633bd6ddfad42169\` ON \`session\``);
-        await queryRunner.query(`DROP TABLE \`session\``);
-        await queryRunner.query(`DROP INDEX \`REL_013c0803e80018bfe46fca1a98\` ON \`user\``);
-        await queryRunner.query(`DROP INDEX \`IDX_e12875dfb3b1d92d7d7c5377e2\` ON \`user\``);
-        await queryRunner.query(`DROP TABLE \`user\``);
-        await queryRunner.query(`DROP TABLE \`account\``);
-        await queryRunner.query(`DROP TABLE \`avatar_image\``);
     }
 
 }
--- src/db/migrations/index.ts
+++ src/db/migrations/index.ts
@@ -1,4 +1,4 @@
 import { AddAuthSchema1762841505188 } from './1762841505188-AddAuthSchema';
-import { AddCoreSchema1767380042595 } from './1767380042595-AddCoreSchema';
+import { AddCoreSchema1767384355051 } from './1767384355051-AddCoreSchema';
 
-export default [AddAuthSchema1762841505188, AddCoreSchema1767380042595];
+export default [AddAuthSchema1762841505188, AddCoreSchema1767384355051];
